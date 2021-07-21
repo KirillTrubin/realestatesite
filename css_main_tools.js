5621d4224bbb8754c5a101276dc3e82ef19e5dc5
KirillTrubin <Strubbler24@gmail.com>
2021-07-21 22:08:42 +0300

    Initial commit

---
+++ css_main_tools.js
@@ -0,0 +1,23 @@
+/* 
+margin - ВНЕШНИЙ ОТСТУП НА ВСЕХ 4-Х СТОРОНАХ ЭЛЕМЕНТА
+    margin-left, margin-right ... top, bottom - ВНЕШНИЙ ОТСТУП ПО ПАРАМЕТРУ 
+    ПРИНИМАЕМЫЕ ЗНАЧЕНИЯ: 
+        inherit
+        initial
+        unset    
+*/
+
+/*
+padding - ВНУТРЕННИЙ ОТСТУП (ВСЕ 4 СТОРОНЫ ЭЛЕМЕНТА)
+    padding-left, padding-right ... top, bottom - ВНУТРЕННИЙ ОТСТУП ПО ПАРАМЕТРУ
+    ПРИНИМАЕМЫЕ ЗНАЧЕНИЯ: 
+        inherit
+        initial
+        unset  
+*/
+
+/* 
+    FLEX
+        justify-content - align on x
+        align-items - align on y
+*/
---
+++ script.js
@@ -0,0 +1,31 @@
+confirm("Are you 18?");
+
+let stcard1 = document.getElementById("stc1")
+let sbut1 = document.getElementById("stbut1")
+
+stcard1.onmouseover = function(){
+    sbut1.style.borderBottom = "4px solid purple";
+}
+stcard1.onmouseout = function(){
+    sbut1.style.borderBottom = "4px solid black";    
+} 
+
+let stcard2 = document.getElementById("stc2")
+let sbut2 = document.getElementById("stbut2")
+
+stcard2.onmouseover = function(){
+    sbut2.style.borderBottom = "4px solid purple";
+}
+stcard2.onmouseout = function(){
+    sbut2.style.borderBottom = "4px solid black";    
+}
+
+let stcard3 = document.getElementById("stc3")
+let sbut3 = document.getElementById("stbut3")
+
+stcard3.onmouseover = function(){
+    sbut3.style.borderBottom = "4px solid purple";
+}
+stcard3.onmouseout = function(){
+    sbut3.style.borderBottom = "4px solid black";    
+} 
